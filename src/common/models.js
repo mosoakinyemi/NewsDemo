7addb9800c4fd000349ee74c60a5063232bd6f35
Andrew Tiema <andrewtiema@gmail.com>
2021-06-29 14:27:40 +0300

    api reusable function

--- src/common/models.js
+++ src/common/models.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import handleApi from '../network/Network';
 
 const baseURL = 'https://5e4bfc87a641ed0014b02740.mockapi.io/api/clane';
 
@@ -59,74 +60,78 @@ export const news = {
     async fetchNewsData(_, state) {
       const {currentPage} = state.news;
       this.toggleLoader(true);
-      axios
-        .request({
-          method: 'get',
-          url: `${baseURL}/news?page=${currentPage}&limit=10`,
-          timeout: 5000,
-        })
-        .then((response) => {
+
+      let endpoint = `${baseURL}/news?page=${currentPage}&limit=10`;
+      handleApi(
+        endpoint,
+        'get',
+        null,
+        function (response) {
           this.toggleLoader(false);
           this.saveNewsData(response.data);
-        })
-        .catch((e) => {
+        },
+        function (e) {
           this.toggleLoader(false);
           console.log(e);
-        });
+        },
+      );
     },
 
     async fetchNewsImages(newsId) {
       this.toggleLoader(true);
-      axios
-        .request({
-          method: 'get',
-          url: `${baseURL}/news/${newsId}/images`,
-          timeout: 5000,
-        })
-        .then((response) => {
+
+      let endpoint = `${baseURL}/news/${newsId}/images`;
+      handleApi(
+        endpoint,
+        'get',
+        null,
+        function (response) {
           this.toggleLoader(false);
           this.saveNewsImages(response.data);
-        })
-        .catch((e) => {
+        },
+        function (e) {
           this.toggleLoader(false);
           console.log(e);
-        });
+        },
+      );
     },
 
     async fetchNewsComments(newsId) {
       this.toggleLoader(true);
-      axios
-        .request({
-          method: 'get',
-          url: `${baseURL}/news/${newsId}/comments`,
-          timeout: 5000,
-        })
-        .then((response) => {
+
+      let endpoint = `${baseURL}/news/${newsId}/comments`;
+      handleApi(
+        endpoint,
+        'get',
+        null,
+        function (response) {
           this.toggleLoader(false);
           this.saveNewsComments(response.data);
-        })
-        .catch((e) => {
+        },
+        function (e) {
           this.toggleLoader(false);
           console.log(e);
-        });
+        },
+      );
     },
     async deleteComment(data) {
       const {newsId, commentId} = data;
       this.toggleLoader(true);
-      axios
-        .request({
-          method: 'delete',
-          url: `${baseURL}/news/${newsId}/comments/${commentId}`,
-          timeout: 5000,
-        })
-        .then((response) => {
+
+      let endpoint = `${baseURL}/news/${newsId}/comments/${commentId}`;
+      handleApi(
+        endpoint,
+        'delete',
+        null,
+        function (res) {
           this.toggleLoader(false);
           this.fetchNewsComments(newsId);
-        })
-        .catch((e) => {
+        },
+        function (e) {
           this.toggleLoader(false);
           console.log(e);
-        });
+        },
+      );
     },
     async postComment(data, state) {
       const {newsId, name, comment} = data;
@@ -137,21 +142,20 @@ export const news = {
         avatar: 'https://picsum.photos/200',
         comment: comment,
       };
-      axios
-        .request({
-          method: 'post',
-          url: `${baseURL}/news/${newsId}/comments`,
-          data: commentData,
-          timeout: 5000,
-        })
-        .then((response) => {
+      let endpoint = `${baseURL}/news/${newsId}/comments`;
+      handleApi(
+        endpoint,
+        'post',
+        commentData,
+        function (response) {
           this.toggleLoader(false);
           this.addComment(response.data);
-        })
-        .catch((e) => {
+        },
+        function (e) {
           this.toggleLoader(false);
           console.log(e);
-        });
+        },
+      );
     },
     async updateComment(data) {
       const {newsId, commentIdToEdit, comment} = data;
@@ -159,21 +163,20 @@ export const news = {
       var updatedComment = {
         comment: comment,
       };
-      axios
-        .request({
-          method: 'put',
-          url: `${baseURL}/news/${newsId}/comments/${commentIdToEdit}`,
-          data: updatedComment,
-          timeout: 5000,
-        })
-        .then((response) => {
+      let endpoint = `${baseURL}/news/${newsId}/comments/${commentIdToEdit}`;
+      handleApi(
+        endpoint,
+        'put',
+        updatedComment,
+        function (res) {
           this.toggleLoader(false);
           this.fetchNewsComments(newsId);
-        })
-        .catch((e) => {
+        },
+        function (e) {
           this.toggleLoader(false);
           console.log(e);
-        });
+        },
+      );
     },
     async createNews(data) {
       const {author, title} = data;
@@ -182,22 +185,21 @@ export const news = {
         author: author,
         title: title,
       };
-      axios
-        .request({
-          method: 'post',
-          url: `${baseURL}/news`,
-          data: newNews,
-          timeout: 5000,
-        })
-        .then((response) => {
+      let endpoint = `${baseURL}/news`;
+      handleApi(
+        endpoint,
+        'post',
+        newNews,
+        function (response) {
           this.toggleLoader(false);
           this.fetchNewsData();
           alert('Congrats, you have created a news!');
-        })
-        .catch((e) => {
+        },
+        function (e) {
           this.toggleLoader(false);
           console.log(e);
-        });
+        },
+      );
     },
     async updateNews(data) {
       const {newsId, author, title} = data;
@@ -206,21 +208,20 @@ export const news = {
         author: author,
         title: title,
       };
-      axios
-        .request({
-          method: 'put',
-          url: `${baseURL}/news/${newsId}`,
-          data: updatedNews,
-          timeout: 5000,
-        })
-        .then((response) => {
+      let endpoint = `${baseURL}/news/${newsId}`;
+      handleApi(
+        endpoint,
+        'put',
+        updatedNews,
+        function (response) {
           this.toggleLoader(false);
           this.fetchNewsData();
-        })
-        .catch((e) => {
+        },
+        function (e) {
           this.toggleLoader(false);
           console.log(e);
-        });
+        },
+      );
     },
   },
 };
